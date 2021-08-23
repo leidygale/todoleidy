0f98e6cd208d80370ba3810c4c4e9cd9e74ae0c3
leidygale <78891427+leidygale@users.noreply.github.com>
2021-08-22 21:04:29 -0500

    Add entity

---
+++ todoleidy.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoleidy.Common.Models
+{
+    public class Todo
+    {
+       public DateTime CreatedTime { get; set; }
+       public string TaskDescription  { get; set; }
+       public bool IsCompleted { get; set; }
+    }
+}
---
+++ todoleidy.Common/Responses/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoleidy.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; } // Devuelve el resultado si es true, si es false envia mensaje
+
+        public string Message { get; set; } //En caso que falle, saca mensaje
+
+        public object Result { get; set; } // 
+
+    }
+}
