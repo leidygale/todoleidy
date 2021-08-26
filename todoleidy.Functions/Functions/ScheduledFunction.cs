20fc4a5483a22c0822be833553e2011993e68dbe
leidygale <78891427+leidygale@users.noreply.github.com>
2021-08-25 23:59:34 -0500

    Unit Test, Preparacion prueba unitaria

---
+++ todoleidy.Functions/Functions/ScheduledFunction.cs
@@ -0,0 +1,34 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Threading.Tasks;
+using todoleidy.Functions.Entities;
+
+namespace todoleidy.Functions.Functions
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async Task Run(
+            [TimerTrigger("0 */2 * * * *")] TimerInfo myTimer,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation($"Deleting completed function executed at: {DateTime.Now}"); //Nos dice a que hora se ejecuto
+
+            string filter = TableQuery.GenerateFilterConditionForBool("IsCompleted", QueryComparisons.Equal, true);
+            TableQuery<TodoEntity> query = new TableQuery<TodoEntity>().Where(filter);
+            TableQuerySegment<TodoEntity> completedTodos = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+            int deleted = 0;
+            foreach (TodoEntity completedTodo in completedTodos)
+            {
+                await todoTable.ExecuteAsync(TableOperation.Delete(completedTodo));
+                deleted++;
+            }
+
+            log.LogInformation($"Delete {deleted} items at: {DateTime.Now}");
+        }
+
+    }
+}
